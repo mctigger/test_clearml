58db15d0b07f3556cca4d2f3db9b9a3253ec2059
Tim Joseph <tim.joseph@mctigger.com>
2021-02-08 21:25:23 +0100

    Import torch

--- run_task.py
+++ run_task.py
@@ -5,30 +5,12 @@ from time import sleep
 import os
 import platform
 import distro
+import torch
 
 
 def main():
     task = Task.init(project_name="test", task_name="importlib_test2")
-    """
-    task = Task.create(
-        project_name="test",
-        task_name="importlib_test2",
-        task_type=Task.TaskTypes.training,
-        docker="ubuntu:18.04",
-        add_task_init_call=True,
-        script="./run_task.py",
-        packages=["clearml", "distro"],
-    )    
-    """
-    task.set_base_docker("continuumio/miniconda3:4.9.2")
-    task.execute_remotely()
-    printme = importlib.import_module("some_package.file_to_import").printme
-    printme()
-    print(os.name)  # returns os name in simple form
-
-    print(platform.system())  # returns the base system, in your case Linux
-    print(platform.release())  # returns release version
-    print(distro.linux_distribution())
+    print(torch)
 
 
 if __name__ == "__main__":
